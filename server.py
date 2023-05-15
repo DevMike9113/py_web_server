3e3ca09dc7294f28eb4a26f35ac6418ea20418ee
DevMike9113 <mike@dawntms.com>
2023-05-15 11:40:47 -0400

    server set up and now adding a favicon

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
@@ -6,4 +6,19 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Bla!</p>"
+    return render_template('index.html')
+
+
+@app.route("/favicon.ico")
+def blog():
+    return render_template('charmander.jpg')
+
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "<p>Hello, dogs are good!</p>"
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
