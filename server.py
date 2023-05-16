eade3edbf946b22e4ba915b9ed5b5aa9822f0a91
DevMike9113 <mike@dawntms.com>
2023-05-16 17:12:22 -0400

    added routes and a database

--- server.py
+++ server.py
@@ -1,24 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 print(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def my_home():
     return render_template('index.html')
 
 
-@app.route("/favicon.ico")
-def blog():
-    return render_template('charmander.jpg')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route("/blog/2020/dogs")
-def blog2():
-    return "<p>Hello, dogs are good!</p>"
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}: {subject} -- {message}')
+        print(file)
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. Try again.'
