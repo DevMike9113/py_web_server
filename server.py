d8dc62982b3dbd53d7f92067167693916cabe312
DevMike9113 <mike@dawntms.com>
2023-05-19 11:45:23 -0400

    pushing data to csv

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -23,11 +24,26 @@ def write_to_file(data):
         print(file)
 
 
+# CSV actually stands for ::: comma separated values
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+        print(csv_writer)
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            # write_to_file(data)
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
     else:
         return 'something went wrong. Try again.'
